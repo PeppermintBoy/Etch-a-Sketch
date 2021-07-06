8180c626c97b410a6bf2d0937e4766eb83250bef
yoyo <yoyointheho@gmail.com>
2021-07-06 12:34:53 +0900

    Added randomize color when mouse hover.But only black to white color

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,31 @@
+const container = document.querySelector('.container');
+const newDiv = document.createElement('div');
+newDiv.classList.add('box');
+
+
+//Fill grid with boxes
 function makeDiv() {
-    const container = document.querySelector('.container');
-    const div = document.createElement('div');
-    div.classList.add('box');
     for (let i = 0; i < 256; i++) {
-        container.appendChild(div.cloneNode(true));  //cloning is needed because element originally created can only be at one place.
+        container.appendChild(newDiv.cloneNode(true));  //cloning is needed because element originally created can only be at one place.
     }
 } 
 
+//return a random color
+function randomizeColor(e) {
+    const r = Math.floor(Math.random() * 255);
+    const color = `rgb(${r}, ${r}, ${r})`;
+    e.style.backgroundColor = color;
+}
+
+//Listens to mouse hover and then prompt a function
+function hover() {
+    const divs = document.querySelectorAll('.box');
+    divs.forEach(div => div.addEventListener('mouseover', e => {
+        //console.log(e.target); //Same value as obtained from querySelector
+        randomizeColor(e.target);
+    }));    
+}
+
 makeDiv();
+hover();
 
